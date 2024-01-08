e6b0bfd1b7d97d60f2f10bd7cf9b2e2e4aa6bc2d
syorito-hatsuki <enderdron3@gmail.com>
2024-01-08 10:28:23 +0200

    Added spaces between cards

--- app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/ui/components/ProjectItem.kt
+++ app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/ui/components/ProjectItem.kt
@@ -1,27 +1,40 @@
 package dev.syoritohatsuki.fstatsmobile.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun ProjectItem(name: String) {
+fun ProjectItem(projectName: String, ownerName: String) {
     Card(
-        modifier = Modifier
-            .padding(4.dp)
-            .fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
     ) {
-        Text(
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxWidth(),
-            text = name,
-            textAlign = TextAlign.Center
-        )
+        Column(modifier = Modifier
+            .clickable {}
+            .padding(8.dp)) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                overflow = TextOverflow.Ellipsis,
+                fontSize = 20.sp,
+                text = projectName,
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                overflow = TextOverflow.Ellipsis,
+                text = ownerName
+            )
+        }
     }
 }
--- app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/ui/screens/ProjectsScreen.kt
+++ app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/ui/screens/ProjectsScreen.kt
@@ -1,12 +1,14 @@
 package dev.syoritohatsuki.fstatsmobile.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import dev.syoritohatsuki.fstatsmobile.data.api.FStatsApi
 import dev.syoritohatsuki.fstatsmobile.data.dto.Project
 import dev.syoritohatsuki.fstatsmobile.ui.components.ProjectItem
@@ -21,19 +23,17 @@ fun ProjectsScreen() {
 
     LaunchedEffect(key1 = "") {
         projects.clear()
-        api.getProjects().apply {
-            println("API: ${this.size}")
-            projects.addAll(this)
-        }
+        projects.addAll(api.getProjects())
     }
 
     LazyColumn(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
     ) {
         projects.forEach {
             item {
-                ProjectItem(it.name)
+                ProjectItem(it.name, it.owner.username)
             }
         }
     }
