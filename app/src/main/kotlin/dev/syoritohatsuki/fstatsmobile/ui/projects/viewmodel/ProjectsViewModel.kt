ebdd494c8a01fed3d58033e331b4e5c0471a3a2c
syorito-hatsuki <enderdron3@gmail.com>
2024-01-22 20:39:09 +0200

    Added test ViewModel

---
+++ app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/ui/projects/viewmodel/ProjectsViewModel.kt
@@ -0,0 +1,30 @@
+package dev.syoritohatsuki.fstatsmobile.ui.projects.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.syoritohatsuki.fstatsmobile.data.api.FStatsApi
+import dev.syoritohatsuki.fstatsmobile.data.dto.Project
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.koin.java.KoinJavaComponent
+
+class ProjectsViewModel : ViewModel() {
+
+    private val api by KoinJavaComponent.inject<FStatsApi>(FStatsApi::class.java)
+
+    private val _movies = MutableStateFlow<List<Project>>(emptyList())
+    val movies: StateFlow<List<Project>> = _movies
+
+    init {
+        fetchProjects()
+    }
+
+    private fun fetchProjects() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val fetchedMovies = api.getProjects()
+            _movies.emit(fetchedMovies)
+        }
+    }
+}
