07b9f75476807d32292a9eeed814ca027962d00b
syorito-hatsuki <enderdron3@gmail.com>
2024-01-25 14:00:16 +0200

    AGP updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("com.android.library") version "8.2.1" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
     kotlin("android") version "1.7.20" apply false
     kotlin("plugin.serialization") version "1.7.20" apply false
 }
