72f151d9ae69105dfa685fecd64941df7a10407f
syorito-hatsuki <enderdron3@gmail.com>
2024-01-28 19:52:49 +0200

    Implemented login

--- app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/login/LoginScreen.kt
+++ app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package dev.syoritohatsuki.fstatsmobile.screens.login
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,12 +19,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.syoritohatsuki.fstatsmobile.StoreUserToken
-import kotlinx.coroutines.delay
+import dev.syoritohatsuki.fstatsmobile.data.dto.ApiMessage
+import dev.syoritohatsuki.fstatsmobile.screens.login.viewmodel.LoginViewModel
+import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,24 +39,26 @@ fun LoginScreen() {
     val scope = rememberCoroutineScope()
     val dataStore = StoreUserToken(context)
 
+    val loginViewModel: LoginViewModel = viewModel()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
 
-        var email by remember { mutableStateOf("") }
+        var username by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
         var loading by remember { mutableStateOf(false) }
 
         OutlinedTextField(
-            value = email,
+            value = username,
             placeholder = {
-                Text(text = "Login")
+                Text(text = "Username")
             },
-            onValueChange = { email = it },
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
-            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { username = it },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
+            modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
@@ -60,26 +67,31 @@ fun LoginScreen() {
                 Text(text = "Password")
             },
             onValueChange = { password = it },
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
             modifier = Modifier.fillMaxWidth(),
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            enabled = email.length > 3 && password.length > 3 && !loading,
+            enabled = username.length >= 3 && password.length >= 3 && !loading,
             onClick = {
                 scope.launch {
-                    loading = !loading
-                    delay(2000)
-                    loading = !loading
+                    loading = true
+                    loginViewModel.login(username, password).catch {
+                        Toast.makeText(
+                            context,
+                            (it as ClientRequestException).response.body<ApiMessage>().message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }.onCompletion {
+                        loading = false
+                    }.collect { token ->
+                        dataStore.saveToken(token)
+                    }
                 }
             },
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(
-                style = MaterialTheme.typography.titleSmall,
-                color = Color.White,
-                text = if (loading) "Loading..." else "Login",
-            )
+            Text(text = if (loading) "Loading..." else "Login")
         }
     }
 }
--- app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/login/viewmodel/LoginViewModel.kt
+++ app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/login/viewmodel/LoginViewModel.kt
@@ -1,5 +1,15 @@
 package dev.syoritohatsuki.fstatsmobile.screens.login.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dev.syoritohatsuki.fstatsmobile.data.api.FStatsApi
+import kotlinx.coroutines.flow.flow
+import org.koin.java.KoinJavaComponent
 
-class LoginViewModel : ViewModel()
+class LoginViewModel : ViewModel() {
+    private val api by KoinJavaComponent.inject<FStatsApi>(FStatsApi::class.java)
+
+    fun login(username: String, password: String) = flow {
+        val login = api.login(username, password)
+        emit(login.token)
+    }
+}
