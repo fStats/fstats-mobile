f8e933977996989683a342364a5c821fbb0957f0
syorito-hatsuki <enderdron3@gmail.com>
2024-02-01 01:59:58 +0200

    Added search for projects list

--- app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/projects/ProjectsScreen.kt
+++ app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/projects/ProjectsScreen.kt
@@ -1,33 +1,66 @@
 package dev.syoritohatsuki.fstatsmobile.screens.projects
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import dev.syoritohatsuki.fstatsmobile.screens.projects.components.ProjectItem
 import dev.syoritohatsuki.fstatsmobile.screens.projects.viewmodel.ProjectsViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProjectsScreen(navController: NavHostController) {
 
     val projectsViewModel: ProjectsViewModel = viewModel()
+    var projectName by remember { mutableStateOf("") }
 
     val projects by projectsViewModel.projects.collectAsState()
+    val filteredProjects = projects.filter { it.name.lowercase().contains(projectName.lowercase()) }
 
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
-    ) {
-        items(projects) {
-            ProjectItem(navController, it)
+    Column {
+        OutlinedTextField(
+            value = projectName,
+            singleLine = true,
+            placeholder = {
+                Text(text = "Project name", color = Color.LightGray)
+            },
+            onValueChange = {
+                projectName = it
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            items(filteredProjects) {
+                ProjectItem(navController, it)
+            }
         }
     }
 }
