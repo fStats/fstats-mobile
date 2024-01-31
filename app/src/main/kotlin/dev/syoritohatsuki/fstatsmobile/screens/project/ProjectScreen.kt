8ca323062dbdd31a47323ff8e5bc18e4071b24ce
syorito-hatsuki <enderdron3@gmail.com>
2024-02-01 01:41:01 +0200

    Added fallback texts for empty charts

--- app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/project/ProjectScreen.kt
+++ app/src/main/kotlin/dev/syoritohatsuki/fstatsmobile/screens/project/ProjectScreen.kt
@@ -1,17 +1,24 @@
 package dev.syoritohatsuki.fstatsmobile.screens.project
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
@@ -46,18 +53,38 @@ fun ProjectScreen(navController: NavController, navBackStackEntry: NavBackStackE
 
     Scaffold(
         topBar = {
-            LineChart(projectLine)
+            if (projectLine.metricLine.isNotEmpty()) {
+                LineChart(projectLine)
+            } else Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Project don't have data yet for display")
+            }
         },
         content = {
-            HorizontalPager(state = pagerState) { page ->
-                type.value = projectPie.metricPie.keys.elementAtOrElse(page) { "" }
-                projectPie.metricPie[type.value]?.let { values ->
-                    PieChart(values, it)
+            if (projectPie.metricPie.isNotEmpty()) {
+                HorizontalPager(state = pagerState) { page ->
+                    type.value = projectPie.metricPie.keys.elementAtOrElse(page) { "" }
+                    projectPie.metricPie[type.value]?.let { values ->
+                        PieChart(values, it)
+                    }
                 }
+            } else Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Project don't have data for last 30 minute to display")
             }
         },
         bottomBar = {
-            PagerNavigator(type, pagerState)
+            if (projectPie.metricPie.isNotEmpty()) PagerNavigator(type, pagerState)
         }, modifier = Modifier.fillMaxSize()
     )
 }
